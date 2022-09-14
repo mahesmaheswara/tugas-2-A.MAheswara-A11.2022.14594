f8f39cfea1f3b91bf022425091c6c413e8c2569b
Maheswara <113037933+mahesmaheswara@users.noreply.github.com>
2022-09-15 01:14:02 +0700

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int u = 18;
+float t = 160;
+char ub = 'L';
+string ubs = "XL";
+int a;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+     //printf ("Project")
+     /*nama Anggara Maheswara
+       nim A11.2022.14594
+    */
+
+
+    cout << "Umur saya : " << u;
+    cout << endl;
+    cout << "Tinggi badan : " << t;
+    cout << "\nUkurann baju : " << ubs;
+
+    cout << "\nMasukan angka";
+    cin >> a;
+    cout << "Angka saya : " << a;
+
+    return 0;
+}
